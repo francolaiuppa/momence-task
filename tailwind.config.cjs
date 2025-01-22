b34aedf08ff5e6f5af93dae5b00684ab548eaaf1
Fran Laiuppa <hello@francolaiuppa.com>
2025-01-22 09:40:33 +0100

    Added shimmering CSS animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
         electric: '#db00ff',
         ribbon: '#0047ff',
       },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite',
+      },
+      keyframes: {
+        shimmer: {
+          '0%': { backgroundPosition: '-200% 0' },
+          '100%': { backgroundPosition: '200% 0' },
+        },
+      },
     },
   },
   plugins: [],
